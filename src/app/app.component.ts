d2eedb64abc68cd775356c78285fbb7766cba0f0
hammadahamed <hammadahamed786@gmail.com>
2023-12-19 21:50:06 +0530

    init theme fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chisqaure-home';
   currentTheme: string = 'dark';
 
+  ngOnInit(): void {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
   setTheme() {
-    console.log('clicked');
-    const themeToSet = this.currentTheme == 'dark' ? 'light' : 'dark';
+    const themeToSet = this.currentTheme === 'dark' ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', themeToSet);
     this.currentTheme = themeToSet;
   }
