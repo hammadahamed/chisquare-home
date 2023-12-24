bee3101a3e00ca2907bb990ada378f856674e359
ashwin-bitsathy <58909026+ashwin-bitsathy@users.noreply.github.com>
2023-12-24 16:09:11 +0530

    backend init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ChartComponent } from './common/chart/chart.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent, HomeComponent, SidebarComponent, ChartComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
